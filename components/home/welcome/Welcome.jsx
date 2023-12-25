124996d059bb4f1c7fe72f02044f05f8c0d15a4b
Emmanuel Michael <michochoprogrammer@gmail.com>
2023-12-25 05:27:32 +0100

    updating with the tutorial

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text,
+TextInput, TouchableOpacity,
+Image,
+FlatList,
+ } from 'react-native'
 
 import styles from './welcome.style'
 
