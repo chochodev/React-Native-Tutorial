265abfaf46150031bbcf52eef54eabfedaad1948
Emmanuel Michael <michochoprogrammer@gmail.com>
2023-12-25 06:50:06 +0100

    updating with the tutorial

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,19 +1,21 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { RAPID_API_KEY } from '@env';
 
-const useFetch = () => {
+const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const options = {
     method: 'GET',
-    url: 'https://jsearch.p.rapidapi.com/search',
-    params: {query: 'Python developer in Texas, USA', page: '1', num_pages: '1'},
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      'X-RapidAPI-Key':
-      'cd31a37c95mshc2d86b20eb73193p145d0ejsn3d7380d65048',
+      'X-RapidAPI-Key': rapidApiKey,
       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-    }
+    },
+    params: {query: 'Python developer in Texas, USA', page: '1', num_pages: '1'},
   }
 };
