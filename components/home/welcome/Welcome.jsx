6d6c3ab1380e56c466fbfd9862d18d15eae870f2
Emmanuel Michael <michochoprogrammer@gmail.com>
2023-12-25 05:41:57 +0100

    updating with the tutorial

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
-import { View, Text,
-TextInput, TouchableOpacity,
-Image,
-FlatList,
- } from 'react-native'
-
+import { 
+  View, 
+  Text,
+  TextInput, 
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native'
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Chocho</Text>
+        <Text style9={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
     </View>
   )
 }
