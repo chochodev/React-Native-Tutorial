236dd86b2fddb5227376000f5257422fdf2fe2bc
Emmanuel Michael <michochoprogrammer@gmail.com>
2023-12-27 11:57:10 +0100

    updated with giving up on following the tutorial, api is giving issues and some functions are deprecated, just gonna read his code in master branch, looks easy anyways

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from "react-native";
+import { Stack, useRouter, useSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
 
-const [id] = () => {
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import useFetch from "../../hook/useFetch";
+
+const JobDetails = () => {
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch('job-details', {
+    job_id: params.id
+  })
   return (
-    <div>[id]</div>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowvisible: false, 
+          headerBackVisible: false,
+          headerLeft: () => {
+            <ScreenHeaderBtn
+            // iconUrl={}
+            />
+          }
+        }}
+        />
+    </SafeAreaView>
   )
 }
 
-export default [id]
+export default JobDetails;
